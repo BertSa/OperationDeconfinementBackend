add95a2103bdf645b5b851753cd579e056f0deb0
BertSa <samuel.b.prog@gmai.com>
2021-02-24 02:40:48 -0500

    first commit

---
+++ src/main/java/ca/bertsa/cal/h21_420_445/operation_deconfinement/OperationDeconfinementApplication.java
@@ -0,0 +1,13 @@
+package ca.bertsa.cal.h21_420_445.operation_deconfinement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationDeconfinementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationDeconfinementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/bertsa/cal/h21_420_445/operation_deconfinement/OperationDeconfinementApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bertsa.cal.h21_420_445.operation_deconfinement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationDeconfinementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
